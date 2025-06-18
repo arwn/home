68827f61c92086b162bd44706af4ac396a1b6969
Aren Windham <windham.aren@protonmail.com>
2025-06-18 10:27:19 -0500

    add teleport env vars for work

--- homes/macbook-work.nix
+++ homes/macbook-work.nix
@@ -5,4 +5,10 @@
 
   home.username = "aren.windham";
   home.homeDirectory = "/Users/aren.windham";
+
+  programs.fish.shellInit = ''
+    set -gx TELEPORT_PROXY transporter.ic-ops.com:443
+    set -gx TELEPORT_CLUSTER transporter.ic-ops.com
+    set -gx TELEPORT_USER aren.windham@immuta.com
+  '';
 }
