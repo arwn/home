99997da023be53f18065218f5bfe7d2e6253b271
Aren Windham <windham.aren@protonmail.com>
2025-08-13 11:29:02 -0500

    add mise to work macbook

--- homes/macbook-work.nix
+++ homes/macbook-work.nix
@@ -1,10 +1,14 @@
-{ config, pkgs, shared, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [ ./shared.nix ];
 
   home.username = "aren.windham";
   home.homeDirectory = "/Users/aren.windham";
+
+  home.packages = lib.mkAfter [
+    pkgs.mise
+  ];
 
   programs.fish.shellInit = ''
     set -gx TELEPORT_PROXY transporter.ic-ops.com:443
