5760125900da4f1e9722c77751233c6f1f5fa817
Aren Windham <windham.aren@protonmail.com>
2026-05-01 14:27:06 -0500

    fucking direnv

--- homes/macbook.nix
+++ homes/macbook.nix
@@ -10,4 +10,19 @@
     pkgs.colima
     pkgs.docker
   ];
+
+  programs.direnv = {
+    enable = true;
+    enableFishIntegration = true;
+    nix-direnv = {
+      enable = true;
+    };
+    # direnv 2.37.1's bash test suite hangs on darwin because some
+    # test scenarios contain literal backspace/CR characters in
+    # directory names which trip up macOS filesystem ops. Skip the
+    # check phase on darwin until upstream fixes this.
+    package = pkgs.direnv.overrideAttrs (old: {
+      doCheck = false;
+    });
+  };
 }
